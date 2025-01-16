d253cb75cb24c7eefd7ef49073bbbb4c1dab6cae
rooooomania <rooooomania@gmail.com>
2025-01-16 15:28:15 +0900

    add a message for staging

--- server.py
+++ server.py
@@ -3,19 +3,26 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 import os
 
+
 def hello_world(request):
-    name = os.environ.get('NAME')
+    name = os.environ.get("NAME")
     if name == None or len(name) == 0:
         name = "JMS"
     message = "Hello, " + name + "!\n"
+
+    # Add "in the staging environment" if the environment is ENVIRONMENT=staging
+    environment = os.environ.get("ENVIRONMENT")
+    if environment == "staging":
+        message += "in the staging environment\n"
+
     return Response(message)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
+        config.add_route("hello", "/")
+        config.add_view(hello_world, route_name="hello")
         app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
+    server = make_server("0.0.0.0", port, app)
     server.serve_forever()
-    
